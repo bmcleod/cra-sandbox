d0c07c2315682c04e90e4aba0b78fae6fc6b3ba1
Lincoln Anderson <landerson@352inc.com>
2018-11-20 17:36:49 -0500

    Remove Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-          Hello world
-      </div>
+      <Fragment>
+          Welcome to React
+      </Fragment>
     );
   }
 }
