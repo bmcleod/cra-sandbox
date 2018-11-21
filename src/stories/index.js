3032270412602ce19f5042debc365d40a05a8ece
Lincoln Anderson <landerson@352inc.com>
2018-11-21 15:30:56 -0500

    Install carbon.

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,16 +4,12 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+
+import 'carbon-components/src/components/button/_button.scss';
+import { Button } from 'carbon-components-react';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
