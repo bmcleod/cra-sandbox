b8d101ec92786cf63b4397a0d7aa58765e01343f
Lincoln Anderson <landerson@352inc.com>
2018-11-21 15:32:53 -0500

    Install carbon.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component, Fragment } from 'react';
 
+// import 'carbon-components/src/components/button/_button.scss';
+import { Button } from 'carbon-components-react';
+
 class App extends Component {
   render() {
     return (
       <Fragment>
-          Welcome to React
+        Welcome to React
+        <Button>Hello</Button>
       </Fragment>
     );
   }
