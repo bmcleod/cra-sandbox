c731d1566ee0a8274e7ab1a5aed2b4ea868e610c
Lincoln Anderson <landerson@352inc.com>
2018-11-21 15:25:16 -0500

    Add craco-antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component, Fragment } from 'react';
 
+import { Button } from 'antd';
+
+import './App.less';
+
 class App extends Component {
   render() {
     return (
       <Fragment>
-          Welcome to React
+        Welcome to React
+        <Button type="primary">Primary</Button>
       </Fragment>
     );
   }
