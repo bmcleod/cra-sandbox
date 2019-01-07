fb5e707745c11877be3723724711aad17d737f06
bmcleod <lbmcleod@gmail.com>
2019-01-07 15:15:44 -0500

    feat: Setup text input and buttons for Carbon

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,10 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/stories');
+
+  // automatically import all story js files that end with *.stories.js
+  const req = require.context('../src/stories', true, /\.stories\.js$/);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
+import { boolean, select, text } from '@storybook/addon-knobs';
 import { Welcome } from '@storybook/react/demo';
 
 import '../styles/index.scss';
